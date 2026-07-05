a4b227c5f2b4f6d1e1d38e4bd4e540fc624eaa26
SatyaRameshReddyNallamilli <rameshreddy67749@gmail.com>
2026-07-05 20:06:24 +0530

    Completed backend CRUD operations

--- app.py
+++ app.py
@@ -1,1 +1,64 @@
 print("Welcome to Coding Problem Tracker")
+
+from db import *
+
+while True:
+
+    print("\n===== Coding Problem Tracker =====")
+    print("1. Add Problem")
+    print("2. View Problems")
+    print("3. Search Problem")
+    print("4. Update Status")
+    print("5. Delete Problem")
+    print("6. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+
+        title = input("Title: ")
+        platform = input("Platform: ")
+        difficulty = input("Difficulty: ")
+        topic = input("Topic: ")
+        status = input("Status: ")
+        notes = input("Notes: ")
+
+        add_problem(
+            title,
+            platform,
+            difficulty,
+            topic,
+            status,
+            notes
+        )
+
+    elif choice == "2":
+        view_problems()
+
+    elif choice == "3":
+
+        title = input("Enter Title: ")
+
+        search_problem(title)
+
+    elif choice == "4":
+
+        pid = int(input("Problem ID: "))
+        status = input("New Status: ")
+
+        update_status(pid, status)
+
+    elif choice == "5":
+
+        pid = int(input("Problem ID: "))
+
+        delete_problem(pid)
+
+    elif choice == "6":
+
+        print("Thank you!")
+        break
+
+    else:
+
+        print("Invalid Choice")
--- db.py
+++ db.py
@@ -1,3 +1,4 @@
+
 import psycopg2
 
 try:
@@ -15,3 +16,105 @@ try:
 
 except Exception as e:
     print("❌ Error:", e)
+    
+def add_problem(title, platform, difficulty, topic, status, notes):
+
+    query = """
+    INSERT INTO problems
+    (title, platform, difficulty, topic, status, notes)
+    VALUES (%s,%s,%s,%s,%s,%s)
+    """
+
+    values = (
+        title,
+        platform,
+        difficulty,
+        topic,
+        status,
+        notes
+    )
+
+    cursor.execute(query, values)
+
+    connection.commit()
+
+    print("Problem Added Successfully!")
+
+'''add_problem(
+    "Two Sum",
+    "LeetCode",
+    "Easy",
+    "Arrays",
+    "Solved",
+    "Used HashMap"
+)'''
+
+def view_problems():
+
+    cursor.execute("SELECT * FROM problems")
+
+    rows = cursor.fetchall()
+
+    if len(rows) == 0:
+        print("No Problems Found")
+
+    else:
+
+        print("\n------ Coding Problems ------")
+
+        for row in rows:
+            print(row)
+
+
+def search_problem(title):
+
+    query = """
+    SELECT * FROM problems
+    WHERE title = %s
+    """
+
+    cursor.execute(query, (title,))
+
+    rows = cursor.fetchall()
+
+    if len(rows) == 0:
+        print("Problem Not Found")
+    else:
+        print("\nProblem Found:\n")
+
+        for row in rows:
+            print(row)
+
+#search_problem("Two Sum")
+
+def update_status(problem_id, new_status):
+
+    query = """
+    UPDATE problems
+    SET status = %s
+    WHERE id = %s
+    """
+
+    cursor.execute(query, (new_status, problem_id))
+
+    connection.commit()
+
+    print("Status Updated Successfully!")
+
+#update_status(1, "Revision")
+view_problems()
+
+def delete_problem(problem_id):
+
+    query = """
+    DELETE FROM problems
+    WHERE id = %s
+    """
+
+    cursor.execute(query, (problem_id,))
+
+    connection.commit()
+
+    print("Problem Deleted Successfully!")
+
+#delete_problem(1)
